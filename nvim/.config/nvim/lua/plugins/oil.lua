2b7ad653e1a35c3e1817dc27f8b99414d817c1b7
p6laris <yunissurchy@gmial.com>
2026-05-18 09:54:56 +0300

    oil back to initial state and fixed yazi

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -19,17 +19,4 @@ return {
   dependencies = { { "nvim-mini/mini.icons", opts = {} } },
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
-  config = function(_, opts)
-    require("oil").setup(opts)
-    -- Sync CWD with oil directory
-    vim.api.nvim_create_autocmd("BufEnter", {
-      pattern = "oil://*",
-      callback = function()
-        local dir = require("oil").get_current_dir()
-        if dir then
-          vim.fn.chdir(dir)
-        end
-      end,
-    })
-  end,
 }
